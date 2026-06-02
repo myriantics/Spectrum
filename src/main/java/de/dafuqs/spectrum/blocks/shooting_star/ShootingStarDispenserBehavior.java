d2f7dbd6e9397a7c5b2f3020bd035a8f70072332
myriantics <myriantics@proton.me>
2026-06-02 11:33:46 -0700

    ShootingStarDispenserBehavior now pulls from the dispenser's center position as well as the entity dimensions for more consistent placement

--- src/main/java/de/dafuqs/spectrum/blocks/shooting_star/ShootingStarDispenserBehavior.java
+++ src/main/java/de/dafuqs/spectrum/blocks/shooting_star/ShootingStarDispenserBehavior.java
@@ -11,17 +11,30 @@ import javax.annotation.*;
 
 public class ShootingStarDispenserBehavior extends DefaultDispenseItemBehavior {
 	
+	// offsets for parity with original values
+	private static final double HORIZONTAL_DISPENSE_OFFSET = 0.225;
+	private static final double VERTICAL_DISPENSE_OFFSET = 0.23;
+	
 	@Override
 	public ItemStack execute(BlockSource pointer, ItemStack stack) {
 		Direction direction = pointer.state().getValue(DispenserBlock.FACING);
 		
-		Level world = pointer.level();
+		Level level = pointer.level();
 		ShootingStarItem shootingStarItem = ((ShootingStarItem) stack.getItem());
-		Vec3 spawnPos = new Vec3(pointer.pos().getX() + direction.getStepX() * 1.125D, pointer.pos().getY() + direction.getStepY() * 1.13D, pointer.pos().getZ() + direction.getStepZ() * 1.125D);
-		ShootingStarEntity shootingStarEntity = shootingStarItem.getEntityForStack(world, spawnPos, stack);
+		
+		// spawn shooting star before calculating output position so we can get entity dimensions
+		ShootingStarEntity shootingStarEntity = shootingStarItem.getEntityForStack(level, pointer.center(), stack);
+		
+		// pull from dimensions to make sure we're not inside the dispenser - (step value * (half a block + offset + half of relevant dimension))
+		shootingStarEntity.setPos(
+				shootingStarEntity.getX() + (direction.getStepX() * (0.5 + HORIZONTAL_DISPENSE_OFFSET + shootingStarEntity.getBbWidth() / 2)),
+				shootingStarEntity.getY() + (direction.getStepY() * (0.5 + VERTICAL_DISPENSE_OFFSET + shootingStarEntity.getBbHeight() / 2)),
+				shootingStarEntity.getZ() + (direction.getStepZ() * (0.5 + HORIZONTAL_DISPENSE_OFFSET + shootingStarEntity.getBbWidth() / 2))
+		);
+		
 		shootingStarEntity.setYRot(direction.toYRot());
 		shootingStarEntity.push(direction.getStepX() * 0.4, direction.getStepY() * 0.4, direction.getStepZ() * 0.4);
-		world.addFreshEntity(shootingStarEntity);
+		level.addFreshEntity(shootingStarEntity);
 		
 		stack.shrink(1);
 		return stack;
